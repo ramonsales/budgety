0914191ef90ca2e952b36fe5be45578256b49a90
Ramon Sales <ramon.silvasales@gmail.com>
2018-04-02 00:27:05 -0300

    Clear the input fields after adding a new income/expense

--- app.js
+++ app.js
@@ -102,6 +102,23 @@ var UIController = (function() {
 
         },
 
+        clearFields: function() {
+            var fields, fieldsArr;
+
+            fields = document.querySelectorAll(DOMstrings.inputDescription + ", " + DOMstrings.inputValue);
+
+            // Convert the List that querySelectorAll has returned to an Array
+            fieldsArr = Array.prototype.slice.call(fields);
+
+            fieldsArr.forEach(function(current, index, array) {
+                current.value = "";
+            });
+
+            // Set the focus to the description text input
+            fieldsArr[0].focus();
+
+        },
+
         getDOMstrings: function() {
             return DOMstrings;
         }
@@ -136,13 +153,16 @@ var controller = (function(budgetCtrl, UICtrl) {
     
         // 2. Add the item to the budget controller
         var newItem = budgetController.addItem(input.type, input.description, input.value);
-    
+        
         // 3. Add the new item to the UI
         UICtrl.addListItem(newItem, input.type);
         
-        // 4. Calculate the budget
+        // 4. Clear input fields
+        UICtrl.clearFields();
+
+        // 5. Calculate the budget
     
-        // 5. Display the budget on the UI
+        // 6. Display the budget on the UI
 
     };
 
